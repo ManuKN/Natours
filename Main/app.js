2bd4d8b505f3cc0833953be4ab9fc1efb0af7779
Manu K N <201910101325@presidencyuniversity.in>
2024-03-26 23:17:48 +0530

    CRUD operation using HTTP methods

--- Main/app.js
+++ Main/app.js
@@ -17,6 +17,28 @@ app.get('/app/v1/tours',(req,res) =>{
     })
 })
 
+app.get('/app/v1/tours/:id',(req,res) =>{
+    console.log(req.params)
+    const id = req.params.id * 1;
+    const tour = tours.find(el => el.id === id)
+    // if(id > tours.length){
+        if(!tour){
+        res.status(404).json({
+            status:'Fail',
+            data:({
+                message:'Invalid ID'
+            })
+        })
+    }
+    
+    res.status(200).json({
+        status:'success',
+        data:{
+            tour
+        }
+    })
+})
+
 app.post('/app/v1/tours',(req,res) =>{
     // console.log(req.body)
 
@@ -31,6 +53,42 @@ app.post('/app/v1/tours',(req,res) =>{
                 tours:newtour
             }
         })
+    })
+})
+
+app.patch('/app/v1/tours/:id',(req,res) =>{
+    const id = req.params.id * 1
+    if(id > tours.length){
+        res.status(404).json({
+            status:'Fail',
+            data:{
+                message:'Invalid Id'
+            }
+        })
+    } 
+
+    res.status(200).json({
+        status:'Success',
+        data:{
+         message:"Updated Data bro  "
+        }
+    })
+})
+
+app.delete('/app/v1/tours/:id',(req,res) =>{
+    const id = req.params.id * 1
+    if(id > tours.length){
+        res.status(404).json({
+            status:'Fail',
+            data:{
+                message:'Invalid Id'
+            }
+        })
+    } 
+
+    res.status(204).json({
+        status:'Success',
+        data:null
     })
 })
 
