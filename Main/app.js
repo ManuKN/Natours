428efaaf74ae6d1d31f8ac71701f4b0a824d6842
Manu <manu.kn@myn.team>
2024-06-18 18:16:51 +0530

    added pug templets to the Natours app

--- Main/app.js
+++ Main/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const morgan = require('morgan');
+const path = require('path');
 // eslint-disable-next-line import/no-extraneous-dependencies
 const rateLimiter = require('express-rate-limit');
 const helmet = require('helmet');
@@ -13,11 +14,13 @@ const reviewRouter = require('./routes/reviewRoutes');
 const globalErrorHandler = require('./controllers/errorController');
 
 const app = express();
+app.set('view engine', 'pug');
+app.set('views', path.join(__dirname, 'views'));
 
 //Global MiddleWares
 
 //helmet middleware
- app.use(helmet());
+app.use(helmet());
 
 //Development logging
 if (process.env.NODE_ENV === 'development') {
@@ -34,7 +37,7 @@ app.use('/app', limiter);
 
 //Body parser , reading data from body into req.body
 //important point , this middleware here below reads the data into req.body and then only after that we can clean the data
-app.use(express.json({limit:'10kb'})); //here we r limit the data that comes in the req.body to 10kb
+app.use(express.json({ limit: '10kb' })); //here we r limit the data that comes in the req.body to 10kb
 //1)middleware to protect against noSql query injection
 app.use(mongosanitize());
 
@@ -42,10 +45,22 @@ app.use(mongosanitize());
 app.use(xss());
 
 //Prevent parameter pollution
-app.use(hpp({ whitelist: ['duration','ratingsQuantity','ratingsAverage','maxGroupSize','difficulty','price'] }));
+app.use(
+  hpp({
+    whitelist: [
+      'duration',
+      'ratingsQuantity',
+      'ratingsAverage',
+      'maxGroupSize',
+      'difficulty',
+      'price',
+    ],
+  }),
+);
 
 //Serving static files
-app.use(express.static(`${__dirname}/public`));
+// app.use(express.static(`${__dirname}/public`));
+app.use(express.static(path.join(__dirname, 'public')));
 app.use((req, res, next) => {
   console.log('Hello i am Middleware');
   next();
@@ -58,15 +73,21 @@ app.use((req, res, next) => {
 });
 
 //Routes
+app.get('/',(req , res) => {
+  res.status(200).render('base',{
+    tour:'Forest',
+    user:'Manu'
+  })
+})
 app.use('/app/v1/tours', tourRouter);
 app.use('/app/v1/users', userRouter);
-app.use('/app/v1/review',reviewRouter);
+app.use('/app/v1/review', reviewRouter);
 
 // app.post('/',(req,res) =>{
 //    res.send('U can Post any data from this URl')
 // })
 
-app.all('*',(req , res , next) =>{
+app.all('*', (req, res, next) => {
   // res.status(404).json({
   //   status:'Fail',
   //   message:`Can't find ${req.originalUrl} on this server!`
@@ -77,12 +98,12 @@ app.all('*',(req , res , next) =>{
   // err.statusCode = 404
 
   //here if we pass err as arumnet to next then express will consider that there is error so that it willk skip all next middleware and directly move to global middlware
-  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404))
-  //calling AppError and sending errors so that express will direct to global middle ware 
-})
+  next(new AppError(`Can't find ${req.originalUrl} on this server!`, 404));
+  //calling AppError and sending errors so that express will direct to global middle ware
+});
 
-app.use(globalErrorHandler)
-//calling global middleware 
+app.use(globalErrorHandler);
+//calling global middleware
 
 //Start Server
 module.exports = app;
