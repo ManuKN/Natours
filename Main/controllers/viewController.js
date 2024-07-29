55f2b3bb801f90c3caecf83e484b9da583162abd
Manu <manu.kn@myn.team>
2024-07-29 18:13:59 +0530

    Changes added the tour pug page

--- Main/controllers/viewController.js
+++ Main/controllers/viewController.js
@@ -1,11 +1,20 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsych = require('../utils/catchAsych')
+
+exports.getOverview = catchAsych(async(req, res) => {
+
+  const tours = await Tour.find();
   res.status(200).render('overview', {
     title: 'All Tours',
+    tours
   });
-};
+})
 
-exports.getTour = (req, res) => {
+exports.getTour = catchAsych(async(req, res) => {
+const tour = await Tour.findOne({slug:req.params.slug}).populate({path: 'reviews' , fields:'review rating user'});
+console.log('selcted tour',tour)
   res.status(200).render('tour', {
     title: 'The Forest Hiker Tour',
+    tour
   });
-};
+});
